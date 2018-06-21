fac7891f29c22e5cc4f9a8ecc37f64899b277671
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2018-06-21 10:34:11 +0200

    mods

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3({ region: 'eu-west-1' });
-const gm = require('gm').subClass({ imageMagick:true });
+const gm = require('gm').subClass({ imageMagick: true });
 const { createReadStream, createWriteStream, writeFileSync } = require('fs');
 
 const getColorChangePercentage = () => {
@@ -8,7 +8,7 @@ const getColorChangePercentage = () => {
   for (let i = 0; i < 3; i++) {
     const randInt = Math.floor(Math.random() * 100);
     if (randInt > 75) {
-      out.push(Math.floor(Math.random() * 100) - 25)
+      out.push(Math.floor(Math.random() * 100) - 25);
     } else {
       out.push(randInt);
     }
@@ -17,13 +17,13 @@ const getColorChangePercentage = () => {
 };
 
 //1 minuto ./tmp
-const imageColorize = imageToUse => new Promise(resolve => {
-  resolve([...imageToUse].slice(2).map(image => {
+const imageColorize = imagesToUse => new Promise(resolve => {
+  resolve(imagesToUse.map(image => {
     return [
-      `${image.slice(0, -4)}_colorize.svg`,
-      gm(createReadStream(`./images/${image}`), 'svg.svg')
+      `${image}_colorize.svg`,
+      gm(createReadStream(`./images/${image}.svg`), 'svg.svg')
         .background('transparent')
-        .colorize(...getColorChangePercentage())
+        .colorize(50, 50, 0)
         .stream('svg')
     ];
   }));
@@ -31,49 +31,13 @@ const imageColorize = imageToUse => new Promise(resolve => {
 
 //=================================== main
 
-const data = require('./mobData.json');
-const mobDataEntries = Object.entries(data);
-const { id } = data;
-const mobData = [...mobDataEntries].slice(1);
-const mobDataLen = mobData.length;
-const imageToUse = mobDataEntries.map(([, fileName]) => fileName);
+const { id, layers } = require('./mobData.json');
 
-let value = gm(createReadStream(`./images/${mobData[0][1]}`), 'svg.svg')
-  .background('transparent');
-
-imageColorize(imageToUse)
-  .then(out => {
-    out.forEach(([fileName, stream]) => {
-      const chunks = [];
-      stream.on('data', data => chunks.push(data));
-      stream.once('end', () => writeFileSync(`./images/${fileName}`, Buffer.concat(chunks)));
-    });
-
-    mobData.forEach(([, image], index) => {
-      if (index !== mobDataLen - 1) {
-        value = value
-          .composite(`./images/${mobData[index + 1][1].slice(0, -4)}_colorize.svg`)
-          .background('transparent');
-      }
-      else {
-        value
-          .stream('svg')
-          .pipe(createWriteStream(`./images/Sprite-${id}.svg`))
-      }
-    })
-  })
-  .catch(console.error);
-
-    /*value
-      .toBuffer('SVG', (err, buffer) => {
-        /!*if(err) console.error(err);
-        const putParams = {
-          Key: `${id}.svg`,
-          Bucket: 'cryptomon',
-          Body: buffer
-        };
-        s3.putObject(putParams)
-          .promise()
-          .then(_ => console.log('cryptomon created'))
-          .catch(console.error)
-      });*!/*/
+const background = layers.shift();
+layers.reduce((acc, layer) => {
+  return acc
+    .background('transparent')
+    .composite(`./images/${layer}.svg`);
+}, gm(`./images/${background}.svg`))
+  .stream('jpg')
+  .pipe(createWriteStream('./images/test.jpg'));
