7c8fb5021e52826a0fc89dac9f0dc8b3eccdbd43
Ng2k <nicola.ng2k@gmail.com>
2018-07-23 14:13:13 +0200

    svg-sprite. Create sprite from many svg images

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 const AWS = require('aws-sdk');
-const s3 = new AWS.S3({
-  region: 'eu-west-1'
-});
+const s3 = new AWS.S3({ region: 'eu-west-1' });
 
-const binPath = `${process.env['LAMBDA_TASK_ROOT']}/imagemagick/bin`;
-process.env['LD_LIBRARY_ROOT'] = `${process.env['LAMBDA_TASK_ROOT']}/imagemagick/lib`;
-const gm = require('gm').subClass({ imageMagick: true, appPath: binPath });
+const File = require('vinyl');
+const SVGSpriter = require('svg-sprite');
+const spriter = new SVGSpriter({
+  mode: {
+    symbol: true
+  }
+});
 
 const md5 = require ('md5');
 
-const { createReadStream, writeFileSync } = require('fs');
+const { resolve, join } = require('path');
+const { readFileSync } = require('fs');
 const genomeParser = require('./genomeParser');
 
-const getColorChangePercentage = () => {
+//todo: Aggiungere funzione per modificare i colori dei singoli layers
+/*const getColorChangePercentage = () => {
   const out = [];
   for (let i = 0; i < 3; i++) {
     const randInt = Math.floor(Math.random() * 100);
@@ -23,12 +27,9 @@ const getColorChangePercentage = () => {
     }
   }
   return out;
-};
+};*/
 
 exports.handler = ({ tokenId }, context, callback) => {
-
-  process.env['PATH'] =`${process.env['PATH']}:${binPath}`;
-
   const { id, layers } = genomeParser(tokenId);
   const idKey = md5(id.toString());
 
@@ -38,33 +39,26 @@ exports.handler = ({ tokenId }, context, callback) => {
   }).promise()
     .then(() => console.log('Sprite monster already created'))
     .catch(() => {
-      layers.slice(1)
-        .map(layer =>
-          gm(`./layers/${layer}.svg`)
-            .background('transparent')
-            .colorize(...getColorChangePercentage())
-            .stream('svg'))
-        .forEach((stream, index) => {
-          const chunks = [];
-          stream.on('data', chunk => chunks.push(chunk));
-          stream.once('end', () => writeFileSync(`/tmp/${index}.svg`, Buffer.concat(chunks)));
-        });
+      const cwd = resolve('layers');
 
-      const chunks = [];
-      layers.slice(1).reduce((acc, layer, index) =>
-        gm(acc, '*.svg')
-          .background('transparent')
-          .composite(`/tmp/${index}.svg`)
-          .stream('svg'), createReadStream(`./layers/${layers[0]}.svg`))
-        .on('data', chunk => chunks.push(chunk))
-        .once('end', () => {
+      layers.forEach(layer => {
+        spriter.add(new File({
+          path: join(cwd, `${layer}.svg`),
+          base: cwd,
+          contents: readFileSync(join(cwd, `${layer}.svg`))
+        }))
+      });
+
+      spriter.compile((err, { stack: { sprite: { contents } } }) => {
+        if(err) console.error(err);
+        else
           s3.putObject({
             Key: `monsters/${idKey}.svg`,
             Bucket: 'cryptomon',
-            Body: Buffer.concat(chunks)
+            Body: contents
           }).promise()
-            .then(_ => console.log('upload on s3'))
+            .then(() => console.log(`upload ${idKey}.svg on s3`))
             .catch(console.error);
-        });
+      })
     });
 };
